5e71b44a7af2a7ea80f14b980c453228fd5382de
amir-khusroo <amirkhusroo250@gmail.com>
2025-01-20 00:24:03 +0530

    abc

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,24 +1,8 @@
 package com.driver.test;
 
-import com.driver.A;
-import com.driver.B;
+import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCases {
 
-    @Test
-    public void testClassA() {
-        A objA = new A();
-        String expectedOutput = "Invoking method from class A";
-        assertEquals(expectedOutput, objA.meth());
-    }
-
-    @Test
-    public void testClassB() {
-        B objB = new B();
-        String expectedOutput = "Method is overridden in Extendend class B";
-        assertEquals(expectedOutput, objB.meth());
-    }
 }
